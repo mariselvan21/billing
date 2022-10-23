f60e1a813d2adac54f0d57259b19f16ad93bf89b
c.mariselvan <mari20041999@gmail.com>
2022-10-23 11:20:00 +0530

    add files

--- billing.js
+++ billing.js
@@ -1,22 +1,28 @@
 
 var unitPrices = document.querySelectorAll(".unit_price");
-//console.log(unit);
+// console.log(unitPrices);
 var quantities = document.querySelectorAll(".quantity");
-var calculateButtons = document.querySelectorAll(".button");
+var calculateButtons = document.querySelectorAll(".btn");
+// console.log(calculateButtons);
 var prices = document.querySelectorAll(".amount");
-
-
-
-
+var totalButton=document.querySelector('.totalBtn');
+// console.log(totalButton);
+var totalAmount=document.querySelector('.totalAmt');
+// console.log(totalAmount);
 
 function calculation() {
     var price = unitPrices[this.i].value * quantities[this.i].value;
     prices[this.i].innerText = price;
 }
-
 calculateButtons.forEach((button,index)=>{
     button.addEventListener("click", calculation);
     button.i = index;
 });
+function total(){
+    var total=parseInt(prices[0].innerText)+parseInt(prices[1].innerText)+parseInt(prices[2].innerText)+parseInt(prices[3].innerText)+parseInt(prices[4].innerText);
+    totalAmount.innerText=total;
+}
+    
+totalButton.addEventListener('click',total);
 
 
